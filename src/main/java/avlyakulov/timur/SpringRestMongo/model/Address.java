920ce61f6f4e9d0e59d6dd9e530d8be599b93434
timcol1 <avlyakulovtimur@gmail.com>
2023-12-10 11:47:09 +0200

    Clear extra fields

--- src/main/java/avlyakulov/timur/SpringRestMongo/model/Address.java
+++ src/main/java/avlyakulov/timur/SpringRestMongo/model/Address.java
@@ -12,5 +12,4 @@ public class Address {
     private String country;
     private String city;
     private String postCode;
-    private String building;
 }
--- src/main/java/avlyakulov/timur/SpringRestMongo/model/Student.java
+++ src/main/java/avlyakulov/timur/SpringRestMongo/model/Student.java
@@ -21,8 +21,4 @@ public class Student {
 
     @Indexed(unique = true)
     private String numberPassport;
-
-    private String fullName;
-    private String gender;
-    private String numberPhone;
 }
